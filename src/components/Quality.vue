3b64a386631b199b7fe05f36a9da83d6d0e8be4d
luic1ff <luiciff.employment@gmail.com>
2024-11-22 13:58:46 +0200

    Quality

--- src/components/Quality.vue
+++ src/components/Quality.vue
@@ -5,8 +5,79 @@ export default {
 </script>
 
 <template>
-  <section id="quality">
+  <section id="quality" class="mx-12">
+    <div class="flex flex-col lg:flex-row">
+            <!--left-side-->
+      <div class="flex flex-col gap-12 xl:w-1/4 xl:gap-20">
+        <!--        item-1-->
+        <div class="flex items-start gap-5">
+          <img class="w-20" src="@/assets/images/quality_01.png" alt="quality_image" />
 
+          <div>
+            <h4>Pure Grades</h4>
+            <p>Enjoy the rich, bold flavor of Organic Colombian Coffee, meticulously crafted for a smooth and satisfying experience.</p>
+          </div>
+        </div>
+
+        <!--        item-2-->
+        <div class="flex items-start gap-5">
+          <img class="w-20" src="@/assets/images/quality_02.png" alt="quality_image" />
+
+          <div>
+            <h4>Wide Assortment</h4>
+            <p>Enjoy the rich, bold flavor of Organic Colombian Coffee, meticulously crafted for a smooth and satisfying experience.</p>
+          </div>
+        </div>
+
+        <!--        item-3-->
+        <div class="flex items-start gap-5">
+          <img class="w-20" src="@/assets/images/quality_03.png" alt="quality_image" />
+
+          <div>
+            <h4>Proper Roasting</h4>
+            <p>Enjoy the rich, bold flavor of Organic Colombian Coffee, meticulously crafted for a smooth and satisfying experience.</p>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="hidden xl:w-1/2 xl:grid xl:place-items-center">
+        <img class="w-[70%]" src="@/assets/images/cup.png" alt="quality_cup">
+      </div>
+
+            <!--right-side-->
+      <div class="flex flex-col gap-12 pt-12 lg:pt-0 xl:w-1/4 xl:gap-20">
+        <!--        item-4-->
+        <div class="flex items-start lg:flex-row-reverse lg:text-end gap-5">
+          <img class="w-20" src="@/assets/images/quality_04.png" alt="quality_image" />
+
+          <div>
+            <h4>High Quality</h4>
+            <p>Enjoy the rich, bold flavor of Organic Colombian Coffee, meticulously crafted for a smooth and satisfying experience.</p>
+          </div>
+        </div>
+
+        <!--        item-5-->
+        <div class="flex items-start lg:flex-row-reverse lg:text-end gap-5">
+          <img class="w-20" src="@/assets/images/quality_05.png" alt="quality_image" />
+
+          <div>
+            <h4>Excellent Grinding</h4>
+            <p>Enjoy the rich, bold flavor of Organic Colombian Coffee, meticulously crafted for a smooth and satisfying experience.</p>
+          </div>
+        </div>
+
+        <!--        item-6-->
+        <div class="flex items-start lg:flex-row-reverse lg:text-end gap-5">
+          <img class="w-20" src="@/assets/images/quality_06.png" alt="quality_image" />
+
+          <div>
+            <h4>Awesome Aroma</h4>
+            <p>Enjoy the rich, bold flavor of Organic Colombian Coffee, meticulously crafted for a smooth and satisfying experience.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
